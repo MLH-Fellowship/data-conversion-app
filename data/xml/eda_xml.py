dd223d4226ed9e7d97e07fe98008d1d4450a0f28
Gideon Tong <gideon@gideontong.com>
2021-02-11 09:26:15 -0800

    Reset data folder

--- data/xml/eda_xml.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open("NSIN_Test_Scenario_Export.xml", "rt", encoding="utf-8") as ex:
-    data = ex.read()
-soup = BeautifulSoup(data, 'lxml')
-
-# Shows first generator element and its children
-print(soup.generator)
