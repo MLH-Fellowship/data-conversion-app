afabeefe1a17a3b3f8937fcaaa757d4e553a1191
Gideon Tong <gideon@gideontong.com>
2021-03-05 17:36:06 -0800

    Prepare dump_lookup_table

--- app/scripts/__init__.py
+++ app/scripts/__init__.py
@@ -3,6 +3,9 @@ from json import dump, load
 from typing import TextIO, Union
 
 
+CSV_HEADERS = ('TAG', 'FILE')
+
+
 def load_lookup_table(fp: Union[str, TextIO]) -> dict:
     '''Loads a previously dumped lookup table
 
@@ -26,7 +29,19 @@ def dump_lookup_table(in_fp: Union[str, TextIO], out_fp: Union[str, TextIO]) -> 
     :returns: Data object that was dumped
     :rtype: dict
     '''
-    pass
+    if type(in_fp) is str:
+        in_fp = open(in_fp)
+    if type(out_fp) is str:
+        out_fp = open(out_fp)
+    in_reader = DictReader(in_fp)
+    lookup_table = dict()
+    for row in in_reader:
+        frame = lookup_table
+        for header in CSV_HEADERS:
+            assert header in row, 'Input file is not a recognized format!'
+            if row[header] not in frame:
+                frame[row[header]] = dict()
+                frame = frame[row[header]]
 
 
 if __name__ == '__main__':
