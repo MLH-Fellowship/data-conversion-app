a273abc7a32c934401166e8d529117ae39d24548
JacobK233811 <66180976+JacobK233811@users.noreply.github.com>
2021-03-05 21:01:14 -0500

    Create .scan Converter Methods
    Write all static methods necessary for constructing a .scan ASCII file given XML data.
    
    Use simple if-else conditionals to convert one categorical variable to a similar one.
    
    Process numbers to the proper number of decimal places.

--- app/converter.py
+++ app/converter.py
@@ -154,6 +154,157 @@ class functions:
 
         return "OFF" if modstatus == "false" else "REFERENCE"
 
+    
+    @staticmethod
+    def to_caps(word: str) -> str:
+        '''Convert string to all caps
+
+        :param word: XML tag text
+        :type word: str
+
+        :returns: capslock word
+        :rtype: str
+        '''
+
+        return word.upper()
+
+
+    @staticmethod
+    def format_degree(num: Union[int, float]) -> str:
+        '''Convert degree single/double digit to triple decimal point
+
+        :param num: number of degrees
+        :type num: int or float
+
+        :returns: formatted degrees
+        :rtype: str
+        '''
+        def to_str(f): return f'{f:.3f} DEG'
+        try:
+            return to_str(float(num))
+        except:
+            return to_str(0)
+
+
+    @staticmethod
+    def format_second(num: Union[int, float]) -> str:
+        '''Convert degree single/double digit to five decimal points
+
+        :param num: number of seconds
+        :type num: int or float
+
+        :returns: formatted seconds
+        :rtype: str
+        '''
+        def to_str(f): return f'{f:.5f} SEC'
+        try:
+            return to_str(float(num))
+        except:
+            return to_str(0)
+
+    
+    @staticmethod
+    def dir_abrev(azdirection: str) -> str:
+        '''Convert full direction to abbreviation
+
+        :param num: direction as a word
+        :type num: str
+
+        :returns: direction as an abbreviation
+        :rtype: str
+        '''
+
+        if azdirection == "Clockwise":
+            return "CW"
+        else:
+            return "CCW"
+
+    
+    @staticmethod
+    def motion_overlay(motion: str) -> str:
+        '''Convert elevation scan motion to a2pats scan overlay model
+
+        :param motion: direction of motion
+        :type motion: str
+
+        :returns: scan overlay model
+        :rtype: str
+        '''
+
+        if motion == "Unidirectional":
+            return "NODDING"
+        else:
+            return "OFF"
+
+    
+    @staticmethod
+    def dir_typ(eldirection: str) -> str:
+        '''Convert up/down direction to a2pats scan type
+
+        :param num: direction up/down
+        :type num: str
+
+        :returns: a2pats scan type
+        :rtype: str
+        '''
+
+        if eldirection == "Up" or eldirection == "Down":
+            return "TRIANGULAR"
+        else:
+            return "SAWTOOTH"
+
+    
+    @staticmethod
+    def dir_dir(eldirection: str) -> str:
+        '''Convert up/down direction to vertical/horizontal
+
+        :param num: direction up/down
+        :type num: str
+
+        :returns: a2pats direction
+        :rtype: str
+        '''
+
+        if eldirection == "Up":
+            return "VERTICAL"
+        elif eldirection == "Down": # it is possible that both up and down should correspond to VERTICAL
+            return "HORIZONTAL"
+
+
+    @staticmethod
+    def period_to_hz(num: Union[int, float]) -> str:
+        '''Convert period single/double in seconds to hertz with five decimal points
+
+        :param num: period in seconds
+        :type num: int or float
+
+        :returns: formatted hertz
+        :rtype: str
+        '''
+        def to_str(f): return f'{f:.5f} HZ'
+        try:
+            return to_str(float(1/num))
+        except:
+            return to_str(0)
+
+    
+    @staticmethod
+    def offset_origin(eloffset: int) -> str:
+        '''Convert offset to origin
+
+        :param eloffset: numerical track offset
+        :type eloffset: int
+
+        :returns: top/bottom origin
+        :rtype: str
+        '''
+
+        if eloffset == 0:
+            return "BOTTOM"
+        else:
+            return "TOP"
+        
+
 
 def import_(fp: Union[str, TextIO], classtype=datastore, downgrade_peaceful=True) -> 'datastore':
     '''Import data dynamically
