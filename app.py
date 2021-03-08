252dceb6d2f2513aeaa85b2742106df8fd8e40a9
Gideon Tong <gideon@gideontong.com>
2021-03-08 14:14:01 -0800

    Begin XML tree traversal

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from app import a2pats, ceesim
-from app.converter import convert_to_a2pats
+from app.converter import convert
 from app.importer import import_
 from app.util import config as config_
 from sys import argv
@@ -18,7 +18,7 @@ def convert(input_file: str, output_file: str) -> a2pats:
     :rtype: a2pats
     '''
     input_data = import_(input_file, ceesim)
-    output_data = convert_to_a2pats(input_data)
+    output_data = convert(input_data)
     success = output_data.export(output_file)
     if success:
         return output_data
--- app/importer.py
+++ app/importer.py
@@ -1,7 +1,19 @@
 from app import a2pats, ceesim, datastore, model
 from io import IOBase
 from typing import Iterator, Union, TextIO
-from xml.etree.ElementTree import iterparse
+from xml.etree import ElementTree
+
+
+def traverse_xml_tree(parent: ElementTree.Element, stack_size=0) -> dict:
+    data = dict()
+    for child in parents:
+        if child.text:
+            text = child.text.strip()
+        if text:
+            data[child.tag] = text
+        else:
+            data[child.tag] = traverse_xml_tree(child, stack_size + 1)
+    return data
 
 
 def strip_xml_namespaces(itr: Iterator) -> None:
@@ -27,13 +39,14 @@ def import_a2pats(fp: TextIO) -> a2pats:
 def import_ceesim(fp: TextIO) -> ceesim:
     '''Import a CEESIM file for conversion
     '''
-    itr = iterparse(fp)
+    itr = ElementTree.iterparse(fp)
     strip_xml_namespaces(itr)
     store = ceesim()
-    # TODO: Parse
+    # TODO: Add more models
+    scan_data = model('SCAN', 'type', 'name')
     # TODO: Replace type
     # TODO: Replace name
-    store.models.append(model('SCAN', 'type', 'name'))
+    store.models.append(scan_data)
     return store
 
 
@@ -54,7 +67,7 @@ def import_(fp: Union[str, TextIO], classtype=datastore, downgrade_peaceful=True
     '''
     if downgrade_peaceful:
         if not issubclass(classtype, datastore):
-            # Assume ceesim import
+            # Assume CEESIM import
             classtype = ceesim
         if type(fp) is str:
             try:
