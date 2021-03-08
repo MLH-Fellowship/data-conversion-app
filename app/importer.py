870e08c3e88d05b81b72d3067b537db4dfdd89a6
Gideon Tong <gideon@gideontong.com>
2021-03-08 13:49:37 -0800

    Finish import_ function

--- app/importer.py
+++ app/importer.py
@@ -1,4 +1,5 @@
 from app import a2pats, ceesim, datastore
+from io import IOBase
 from typing import Tuple, Union, TextIO
 
 
@@ -8,13 +9,13 @@ def import_a2pats():
     pass
 
 
-def import_ceesim():
+def import_ceesim(fp: Union[str, TextIO]):
     '''Import a CEESIM file for conversion
     '''
     pass
 
 
-def import_(fp: Union[str, TextIO], classtype=datastore, downgrade_peaceful=True) -> 'datastore':
+def import_(fp: Union[str, TextIO], classtype=datastore, downgrade_peaceful=True) -> datastore:
     '''Import data dynamically
 
     :param fp: File pointer or string to import file
@@ -31,12 +32,28 @@ def import_(fp: Union[str, TextIO], classtype=datastore, downgrade_peaceful=True
     '''
     if downgrade_peaceful:
         if not issubclass(classtype, datastore):
-            classtype = datastore
+            # Assume ceesim import
+            classtype = ceesim
+        if type(fp) is str:
+            try:
+                fp = open(fp)
+            except:
+                return datastore()
+        elif not isinstance(fp, IOBase):
+            return datastore()
     else:
         assert issubclass(
             classtype, datastore), 'Your import_ call must be of datastore or datastore-like type!'
+        if type(fp) is str:
+            fp = open(fp)
+        assert isinstance(
+            fp, IOBase), 'Your import_ call must provide a valid files-like pointer or file path!'
     if classtype is a2pats:
         return import_a2pats(fp)
+    elif classtype is ceesim:
+        return import_ceesim(fp)
+    else:
+        raise ValueError('This type of datastore isn\'t supported yet!')
 
 
 def xml_to_dictionary(filepath: str) -> dict:
